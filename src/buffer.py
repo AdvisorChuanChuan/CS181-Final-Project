89583540f57942e66bbfbc469a0c21e5812b4237
AdvisorChuanChuan <licha@shanghaitech.edu.cn>
2020-12-27 00:15:27 +0800

    update

--- src/buffer.py
+++ src/buffer.py
@@ -1,7 +1,10 @@
 class OrderBuffer:
     def __init__(self):
         self.untreated_orders = []
-        self.recieved_orders = []
+        self.accepted_orders = []
 
-    def pushNewOrder(self, order):
-        self.untreated_orders.append(order)
+    def pushNewOrder(self, _order):
+        self.untreated_orders.append(_order)
+    
+    def acceptOrder(self, _order_idx):
+        
--- src/featureExtractor.py
+++ src/featureExtractor.py
@@ -1,4 +1,7 @@
 class FeatureExtractor:
+    def __init__(self):
+        self.features_num = 5 # TODO
+
     def getFeatures(self, _state, _action):
         # TODO
         pass
