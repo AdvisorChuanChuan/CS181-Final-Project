8a24c7120af2eefe350c385f9363860909677b72
Artanic <599253487@qq.com>
2021-01-06 15:11:55 +0800

    update decoder

--- src/lca_ws/world.py
+++ src/lca_ws/world.py
@@ -7,15 +7,17 @@ import util
 from map import *
 from agent import *
 import copy
+import numpy as np
+
 
 class World:
-    def __init__(self, _map = Mymap()):
+    def __init__(self, _map=Mymap()):
         self.map = _map
         self.agent = ApproximateQAgent(self.getLegalActions, self)
-        self.init_agent_pos = (5,2) # at campus
-        self.start_time = dt.datetime(2020,1,1,11,00)
-        self.end_time = dt.datetime(2020,1,1,13,00)  # 11:00 --- 14:00, 1 min per action
-        self.delta_t = dt.timedelta(seconds=60)    # deltat = 1 min
+        self.init_agent_pos = (5, 2)  # at campus
+        self.start_time = dt.datetime(2020, 1, 1, 11, 00)
+        self.end_time = dt.datetime(2020, 1, 1, 13, 00)  # 11:00 --- 14:00, 1 min per action
+        self.delta_t = dt.timedelta(seconds=60)  # deltat = 1 min
         self.orders_packet_idx = 0
         self.training_df = pd.read_csv("data/" + str(self.orders_packet_idx) + ".csv")
 
@@ -34,8 +36,7 @@ class World:
             "Succeeded orders": [],
             "Failed orders": []
         })
-        csv_data.to_csv('./log/iteration.csv', mode="w", index=True)
-
+        csv_data.to_csv('./log_lca/iteration.csv', mode="w", index=True)
 
     def getImmOrders(self, _str_time):
         """
@@ -58,13 +59,13 @@ class World:
         nextState = []
         # Find next position
         if _action[0] == 'East':
-            nextState.append((_state[0][0], _state[0][1]+1))
+            nextState.append((_state[0][0], _state[0][1] + 1))
         elif _action[0] == 'West':
-            nextState.append((_state[0][0], _state[0][1]-1))
+            nextState.append((_state[0][0], _state[0][1] - 1))
         elif _action[0] == 'South':
-            nextState.append((_state[0][0]+1, _state[0][1]))
+            nextState.append((_state[0][0] + 1, _state[0][1]))
         elif _action[0] == 'North':
-            nextState.append((_state[0][0]-1, _state[0][1]))
+            nextState.append((_state[0][0] - 1, _state[0][1]))
         else:
             nextState.append(_state[0])
         # Find next time
@@ -105,7 +106,7 @@ class World:
             return tuple(nextState), actual_reward + self.getFinalStateValue(nextState)
 
     def getFinalStateValue(self, _state):
-        assert(len(self.getLegalActions(_state)) == 0)
+        assert (len(self.getLegalActions(_state)) == 0)
         exceed_orders_num = 0
         for order in _state[2] + _state[3]:
             if util.str_to_datetime(_state[1]) >= util.getDueTime(order):
@@ -125,23 +126,24 @@ class World:
             return []
 
         successors = self.map.get_successor(_state[0])
-        assert(len(successors)>0)
+        assert (len(successors) > 0)
         # move_actions = ['Stay']
         move_actions = []
-        if (_state[0][0]+1, _state[0][1]) in successors:
+        if (_state[0][0] + 1, _state[0][1]) in successors:
             move_actions.append('South')
-        if (_state[0][0]-1, _state[0][1]) in successors:
+        if (_state[0][0] - 1, _state[0][1]) in successors:
             move_actions.append('North')
-        if (_state[0][0], _state[0][1]+1) in successors:
+        if (_state[0][0], _state[0][1] + 1) in successors:
             move_actions.append('East')
-        if (_state[0][0], _state[0][1]-1) in successors:
+        if (_state[0][0], _state[0][1] - 1) in successors:
             move_actions.append('West')
 
         ImmOrders = self.getImmOrders(_state[1])
         order_actions = util.getHandleOrdersChoices(ImmOrders)
         # Decrease choices according to the orders in current state
         curr_received_num = len(_state[2])
-        order_actions = [valid_action for valid_action in order_actions if curr_received_num + len(valid_action[0]) <= self.max_received_num]
+        order_actions = [valid_action for valid_action in order_actions if
+                         curr_received_num + len(valid_action[0]) <= self.max_received_num]
         actions = []
         for move_action in move_actions:
             for order_action in order_actions:
@@ -153,7 +155,7 @@ class World:
     def isTerminal(self, _state):
         return len(self.getLegalActions(_state)) == 0
 
-    def trainWeights(self, numIter = 100000):
+    def trainWeights(self, numIter=100000):
         """
         Start from an innocent agent, repeat the delivery period for numIter times.
         Actions are chosen arbitrarily.
@@ -180,15 +182,15 @@ class World:
                     print(self.agent.weights)
             # TODO: plot this figure
 
-    def valueIter(self, numIter = 1000):
+    def valueIter(self, numIter=1000):
         # Fill all states
         states = []
         init_state = self.init_agent_state
         queue = util.Queue()
         queue.push(init_state)
         while not queue.isEmpty():
-            if len(states) > 0 and math.log(len(states),10) - int(math.log(len(states),10)) == 0:
-                print("states num = 10 ^ ", math.log(len(states),10))
+            if len(states) > 0 and math.log(len(states), 10) - int(math.log(len(states), 10)) == 0:
+                print("states num = 10 ^ ", math.log(len(states), 10))
                 print(states[-1])
             state = queue.pop()
             states.append(state)
@@ -215,7 +217,7 @@ class World:
                     new_values[state] = max(values_on_action)
             values = new_values.copy()
 
-    def policyIter_TDL(self, improve_times = 1000, epis_per_improve = 100):
+    def policyIter_TDL(self, improve_times=1000, epis_per_improve=100):
         """
         Policy eval(TDL) and improve
         """
@@ -229,7 +231,7 @@ class World:
                 while not self.isTerminal(state):
                     successor, reward = self.getSuccessorStateandReward(state, self.agent.getAction_byDict(state))
                     sample = reward + self.agent.gamma * values_pi[successor]
-                    values_pi[state] = (1-self.agent.alpha) * values_pi[state] + self.agent.alpha * sample
+                    values_pi[state] = (1 - self.agent.alpha) * values_pi[state] + self.agent.alpha * sample
                     state = successor
             # Improve policy
             new_policy = util.Counter()
@@ -252,7 +254,7 @@ class World:
             # if improve_idx % 100 == 0:
             self.testOneEpisode_byDict()
 
-    def random_policy_improvement(self, improve_times = 1000):
+    def random_policy_improvement(self, improve_times=1000):
         for improve_idx in range(improve_times):
             print("improve times: ", improve_idx)
             self.testOneEpisode_byDict()
@@ -260,7 +262,6 @@ class World:
             new_policy = self.agent.policy.copy()
             for state in new_policy:
                 new_policy[state] = rd.choice(self.getLegalActions(state))
-
 
     def record_results(self, _success, _fail, _state, _nextState):
         """
@@ -279,7 +280,6 @@ class World:
                     # find failed orders
                     else:
                         _fail.append(prev_order)
-
 
     def testOneEpisode_byQvalues(self):
         """
@@ -337,6 +337,31 @@ class World:
 
         self.log_iteration(score, success, fail)
 
+    @staticmethod
+    def decoder():
+        with open("./log_lca/policy1.json", "r") as f:
+            b = json.loads(f.read())
+            re = util.Counter()
+            for key, value in b.items():
+                key = json.loads(key)
+                tem_key = []
+                for i in key[2]:
+                    re_tuple = json.loads(i)
+                    tem_key.append((np.dtype('int64').type(re_tuple[0]), re_tuple[1], re_tuple[2],
+                                    np.dtype('int64').type(re_tuple[3])))
+
+                key[2] = tuple(tem_key)
+                tem_key.clear()
+                for i in key[3]:
+                    re_tuple = json.loads(i)
+                    tem_key.append((np.dtype('int64').type(re_tuple[0]), re_tuple[1], re_tuple[2],
+                                    np.dtype('int64').type(re_tuple[3])))
+                key[3] = tuple(tem_key)
+                key[0] = tuple(key[0])
+                key = tuple(key)
+                re[key] = value
+        return re
+
     def log_iteration(self, score, success, fail):
         tem_success = []
         tem_fail = []
@@ -350,11 +375,11 @@ class World:
             "Succeeded orders": [json.dumps(tem_success)],
             "Failed orders": [json.dumps(tem_fail)]
         })
-        csv_data.to_csv('./log/iteration.csv', mode="a", header=False, index=True)
+        csv_data.to_csv('./log_lca/iteration.csv', mode="a", header=False, index=True)
 
         if self.best_score < score:
             self.best_score = score
-            with open('./log/policy.json', 'w') as f:
+            with open('./log_lca/policy.json', 'w') as f:
                 data = self.agent.policy
                 keys = []
                 values = list(data.values())
@@ -371,13 +396,6 @@ class World:
                 f.write(result)
 
 
-
-
-
-
-
-
-
 if __name__ == "__main__":
     zhangjiang = World()
     # zhangjiang.valueIter()
@@ -386,9 +404,3 @@ if __name__ == "__main__":
     # zhangjiang.testOneEpisode_byDict()
 
     zhangjiang.random_policy_improvement()
-
-
-
-
-
-
